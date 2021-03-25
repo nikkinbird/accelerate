93caf1d026e560af45d3c1595da7adc8bdf2987a
nikkinbird <nikki.bollman@gmail.com>
2021-03-24 21:00:57 -0500

    Added fields to single-case-studies.php

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,10 +14,46 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+<div id="primary" class="site-content sidebar">
+<div class="main-content" role="main">
+			
+<?php while ( have_posts() ) : the_post();
+	$services = get_field('services');
+	$client = get_field('client');
+	$link = get_field('site_link');
+	$image_1 = get_field('image_1');
+	$image_2 = get_field('image_2');
+	$image_3 = get_field('image_3');
+	$size = "full"; ?>
+
+<article class="case-study">
+	<aside class="case-study-sidebar">
+		<h2><?php the_title(); ?></h2>
+		<h5><?php echo $services; ?></h5>
+		<h6>Client: <?php echo $client; ?></h6>
+
+		<?php the_content(); ?>
+
+		<p><a href="<?php echo $link; ?>">Site Link</a></p>
+	</aside>
+
+	<div class="case-study-images">
+		<?php if($image_1) {
+			echo wp_get_attachment_image( $image_1, $size );
+		} ?>
+		<?php if($image_2) {
+			echo wp_get_attachment_image( $image_2, $size );
+		} ?>
+		<?php if($image_3) {
+			echo wp_get_attachment_image( $image_3, $size );
+		} ?>
+
+
+	</div>
+
+</article>
+
+
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
