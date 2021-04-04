4edba071b58671214a7b1cec639a6810df7093d2
nikkinbird <nikki.bollman@gmail.com>
2021-04-03 19:11:33 -0500

    Added about page template file

---
+++ wp-content/themes/accelerate-theme-child/page-templates/page_about_template.php
@@ -0,0 +1,22 @@
+<?php  /** Template Name: About Page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<div id="form-wrapper">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+					<?php the_content(); ?>
+				<?php endwhile; // end of the loop. ?>
+		</div>
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
