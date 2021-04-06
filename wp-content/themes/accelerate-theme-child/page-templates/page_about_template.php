230eb0bbd1e8f312a198b7b74c568ae7653587fc
nikkinbird <nikki.bollman@gmail.com>
2021-04-05 20:49:05 -0500

    Added overview custom fields to about page template

--- wp-content/themes/accelerate-theme-child/page-templates/page_about_template.php
+++ wp-content/themes/accelerate-theme-child/page-templates/page_about_template.php
@@ -9,10 +9,23 @@ get_header(); ?>
 
 	<div id="primary" class="about_page hero-content">
 		<div class="main-content about_content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
+			<?php while ( have_posts() ) : the_post();
+					$overview_title = get_field('overview_title');
+					$overview_text = get_field('overview_text'); ?>
+
+			 <div class="about_header_wrapper">
+			 	<div class="about_header">
+					<?php the_content(); ?>
+				</div>
+			</div>
+
+				<div class="overview">
+					<h2><?php echo $overview_title; ?></h2>
+					<p><?php echo $overview_text; ?></p>
+				</div>
+
 			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
+		</div> <!-- .main-content -->
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
