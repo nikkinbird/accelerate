234ea89aa84cff9fa4d8d40773ec86cc6025b7e7
nikkinbird <nikki.bollman@gmail.com>
2021-04-05 20:11:17 -0500

    Styled main content area of about page template

--- wp-content/themes/accelerate-theme-child/page-templates/page_about_template.php
+++ wp-content/themes/accelerate-theme-child/page-templates/page_about_template.php
@@ -7,16 +7,12 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content sidebar">
-		<div class="main-content" role="main">
-			<div id="form-wrapper">
-				<?php while ( have_posts() ) : the_post(); ?>
-					<h2><?php the_title(); ?></h2>
-					<?php the_content(); ?>
-				<?php endwhile; // end of the loop. ?>
-		</div>
+	<div id="primary" class="about_page hero-content">
+		<div class="main-content about_content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php the_content(); ?>
+			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
-
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
