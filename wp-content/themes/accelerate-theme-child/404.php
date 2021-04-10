9e97416d2f1ceb448c9088ed39bcfa8751f904db
nikkinbird <nikki.bollman@gmail.com>
2021-04-10 17:48:31 -0500

    Added 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the 404 page.
+ *
+ 
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div class="main-404">
+		<div class="content-wrapper-404">
+			<h1>Oops! This page doesn't exist!</h1>
+			<p>...but here's a cute baby bunny to look at before you go!</p>
+			<img src="wp-content/themes/accelerate-theme-child/img/bunny.jpg">
+			<p>Photo by <a href="https://unsplash.com/@gcasales?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Guillermo Casales</a> on <a href="https://unsplash.com/s/photos/baby-bunny?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></p>
+		</div>
+
+	</div><!-- .main-404 -->
+
+<?php get_footer(); ?>
