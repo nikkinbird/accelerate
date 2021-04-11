2112c94682044c7bd642d387b1a8891fd7d826c3
nikkinbird <nikki.bollman@gmail.com>
2021-04-10 20:50:27 -0500

    Added Ribeye Marrow font to 404 page headher

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -13,6 +13,7 @@
 function accelerate_child_scripts(){
 	wp_enqueue_style( 'accelerate-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
+    wp_enqueue_style( 'accelerate-child-google-fonts', '//fonts.googleapis.com/css2?family=Ribeye+Marrow&display=swap' );
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
 
